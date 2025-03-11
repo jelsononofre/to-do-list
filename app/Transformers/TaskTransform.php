6d45cf1e727af12b345157311edf5c1dec872e6b
Jelson Onofre <jelson@jettax.com.br>
2025-03-11 20:45:17 -0300

    add transformer

---
+++ app/Transformers/TaskTransform.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Helpers\Utils;
+use App\Models\Task;
+use League\Fractal\TransformerAbstract;
+
+class TaskTransform extends TransformerAbstract
+{
+    public function transform($object)
+    {
+        $statusCode = $object['status'];
+        $createdAt = $object['created_at'];
+        $updatedAt = $object['updated_at'];
+
+        return [
+            'id' => $object['id'],
+            'taskInfo' => [
+                'title' => $object['title'],
+                'info' => $object['info']
+            ],
+            'status' => Task::STATUS[$statusCode],
+            'createdBy' => $object['created_by'],
+            'createdAt' => Utils::formatDate($createdAt),
+            'updatedAt' => Utils::formatDate($updatedAt)
+        ];
+    }
+}
