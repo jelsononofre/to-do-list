894bace0130245656d0bdb26c9d66d56b8833964
Jelson Onofre <jelson@jettax.com.br>
2025-03-11 20:46:49 -0300

    add get all method in controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,27 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\TaskRepository;
+use App\Transformers\TaskTransform;
 use Exception;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
+
 
 class TaskController extends Controller
 {
+    protected Authenticatable $authUser;
+
     public function __construct(
         protected TaskRepository $repository
     ) {
-
+        $this->authUser = auth()->user();
     }
 
     public function create(Request $request)
     {
         try {
-            $user = auth()->user();
-
             $params = [
                 'title' => $request->get('title', null),
                 'info' => $request->get('info', null),
                 'status' => $request->get('status', default: 0),
-                'created_by' => $user->name
+                'created_by' => $this->authUser->name
             ];
     
             $results = $this->repository->create($params);
@@ -49,7 +52,18 @@ class TaskController extends Controller
 
     public function getAll()
     {
+        try {
+            $results = $this->repository->getAll(
+                $this->authUser
+            );
 
+            $transformData = fractal($results, new TaskTransform());
+
+            return response()->json($transformData);
+        } catch (Exception $error) {
+            report($error);
+            dd($error);
+        }
     }
 
     public function update()
