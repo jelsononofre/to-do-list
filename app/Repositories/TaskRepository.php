bab8508e5aa96e5a8bf3fdf1ccb3b559468a8ec6
Jelson Onofre <jelson@jettax.com.br>
2025-03-11 20:46:20 -0300

    add get all method in repository

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -3,11 +3,19 @@
 namespace App\Repositories;
 
 use App\Models\Task;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 class TaskRepository
 {
     public function create(array $params)
     {
         return Task::create($params);
+    }
+
+    public function getAll(Authenticatable $authUser)
+    {
+        return Task::where("created_by", '=', $authUser->name)
+            ->get()
+            ->toArray();
     }
 }
